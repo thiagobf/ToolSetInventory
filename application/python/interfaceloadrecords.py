b42dff0c33b98d89717d07a7273a2334825f7d07
Thiago Bueno Fernandes <fernandestb@gmail.com>
2026-08-09 17:23:02 -0300

    Changes to generate unique code for a tool

--- application/python/interfaceloadrecords.py
+++ application/python/interfaceloadrecords.py
@@ -1,7 +1,12 @@
 import sqlite3
+from datetime import datetime
 from enum import Enum
 
 DB_NAME = r".\database\sqlitedb\toolsetinventory.db"
+
+
+def get_timestamp():
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 class BrandCode(Enum): 
     NONBRAND = "NOB"
@@ -25,47 +30,89 @@ class BrandCode(Enum):
     DREMEL = "DRE" 
     WESTERN = "WES"
 
-def get_brand_code(brand_name):
-    brand_name = brand_name.upper()
-    for brand in BrandCode:
-        if brand.name == brand_name:
-            return brand.value
-    return BrandCode.NONBRAND.value
+class LoadRecords(): 
+    def __init__(self):
+        self.size = 20
+        self.MakerHash =  [[] for _ in range(self.size)]
 
-def generate_tool_code(brand_code, tool_name):
-    tool_name = tool_name.upper()
-    code = f"{brand_code}-{tool_name[:3]}"
-    return code
+    def _GenerateHash(self, KeyMaker):
+        return hash(KeyMaker) % self.size
+    
+    def insertMaker(self, KeyMaker):
+        index = self._GenerateHash(KeyMaker)
+        if self.MakerHash[index] is None:
+            self.MakerHash[index] = []
+        self.MakerHash[index].append(KeyMaker)
 
-def generate_tool_numcode(tool_code,indextool):
-    tool_code = tool_code.upper()
-    code = f"{tool_code}-{indextool:05d}"
-    return code
+    def searchMaker(self, KeyMaker):
+        index = self._GenerateHash(KeyMaker)
+        if self.MakerHash[index] is not None:
+            for maker in self.MakerHash[index]:
+                if maker == KeyMaker:
+                    return True
+        return False
+    
+    def generate_unique_code(self, Maker):
+        index = self._GenerateHash(Maker)
+        if self.MakerHash[index] is None:
+            self.MakerHash[index] = []
+        self.MakerHash[index].append(Maker)
+        unique_code = f"{Maker[:3].upper()}-{len(self.MakerHash[index]):05d}"
+        return unique_code
+    
+    def get_brand_code(self, brand_name):
+        brand_name = brand_name.upper()
+        for brand in BrandCode:
+            if brand.name == brand_name:
+                return brand.value
+        return BrandCode.NONBRAND.value
 
-def LoadFile():
-    RecordsList = []
-    Record = []
-    with open(".\\database\\rawdata\\tools.csv", "r",encoding='utf-8') as file:
-        for line in file:
-            RecordsList.append(line.strip())
-            
-        for Record in RecordsList:
-            Record = Record.split(";")
-            Brand = Record[0].upper()
-            Name = Record[1]
-            Description = Record[2]
-            price = float(Record[3].replace(",","."))
-            loadTools(Brand, generate_tool_numcode(generate_tool_code(get_brand_code(Brand), Name), 1), Name, Description, price)
+    def generate_tool_code(self, brand_code, tool_name):
+        tool_name = tool_name.upper()
+        UniqueCode = self.generate_unique_code(brand_code)
+        return UniqueCode
+
+    def generate_tool_numcode(self, tool_code, indextool):
+        tool_code = tool_code.upper()
+        code = f"{tool_code}-{indextool:05d}"
+        return code
+
+    def loadToolsInDB(self, brand, code, name, description, price):
+        conn = sqlite3.connect(DB_NAME)
+        c = conn.cursor()
+        c.execute("INSERT INTO Tools (brand, code, name, description, price) VALUES (?, ?, ?, ?, ?)", (brand, code, name, description, price))
+        conn.commit()
+        conn.close()
+
+    def LoadFile(self):
+        RecordsList = []
+        Record = []
+        print(f"[{get_timestamp()}] <<Loading records from file>>")
+        with open(".\\database\\rawdata\\tools.csv", "r",encoding='utf-8') as file:
+            for line in file:
+                RecordsList.append(line.strip())
+                
+            for Record in RecordsList:
+                Record = Record.split(";")
+                Brand = Record[0].upper()
+                Name = Record[1]
+                Description = Record[2]
+                price = float(Record[3].replace(",","."))
+                ToolCode = self.generate_tool_code(Brand, Name)
+                self.loadToolsInDB(Brand, ToolCode, Name, Description, price)
 
 
-def loadTools(brand, code, name, description, price):
-    conn = sqlite3.connect(DB_NAME)
-    c = conn.cursor()
-    c.execute("INSERT INTO Tools (brand, code, name, description, price) VALUES (?, ?, ?, ?, ?)", (brand, code, name, description, price))
-    print(f"Inserted: {brand}, {code}, {name}, {description}, {price}")
-    conn.commit()
-    conn.close()
 
 
-LoadFile()
+if __name__ == "__main__":
+    print(f"[{get_timestamp()}] Begin Loading Records")
+    print("*******************************")
+    print(f"[{get_timestamp()}] Instantiating LoadRecords class")
+    LoadRecord = LoadRecords()
+    print(f"[{get_timestamp()}] Loading records from file")
+    LoadRecord.LoadFile()
+    print("*******************************")
+    print(f"[{get_timestamp()}] Loading records completed")
+    print(f"[{get_timestamp()}] End of Loading Records")
+
 #loadTools("Bosch", "BOS-001", "Cordless Drill", "18V Cordless Drill with battery and charger", 99.99)
